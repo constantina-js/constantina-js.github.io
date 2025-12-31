cd91149659f4180052776a93aae7829b898dd4ab
Constantina "Connie" Scoullis <scoullisconstantina@gmail.com>
2025-12-31 00:40:32 -0500

    added updated styling; still working on issues with tag links and tag page

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,9 +2,28 @@ document.addEventListener("DOMContentLoaded", () => {
     const body = document.body;
     const darkLightToggle = document.getElementById('darkModeToggle');
 
-    console.log(darkLightToggle); // now it will log the element
+    if (darkLightToggle) {
 
-    darkLightToggle.addEventListener("change", function () {
-        body.classList.toggle('dark-mode', this.checked);
-    });
+        const savedTheme = localStorage.getItem("theme");
+
+        if (savedTheme === "dark") {
+            body.setAttribute("data-theme", "dark");
+            darkLightToggle.checked = true;
+        } else if (!savedTheme && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+            body.setAttribute("data-theme", "dark");
+            darkLightToggle.checked = true;
+        }
+
+        darkLightToggle.addEventListener("change", () => {
+            if (darkLightToggle.checked) {
+                body.setAttribute("data-theme", "dark");
+                localStorage.setItem("theme", "dark");
+            } else {
+                body.removeAttribute("data-theme");
+                localStorage.setItem("theme", "light");
+            }
+        });
+    
+    }
+
 });
